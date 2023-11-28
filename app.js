a72f95a11880bf908137e736756625e466f9b8ef
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 12:20:43 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+function uploadImage() {
+    const input = document.getElementById('imageInput');
+    const imageContainer = document.getElementById('imageContainer');
+    const puzzleImage = document.getElementById('puzzleImage');
+
+    input.onchange = function (event) {
+        const file = event.target.files[0];
+        const imageURL = URL.createObjectURL(file);
+        puzzleImage.src = imageURL;
+        imageContainer.style.display = 'block';
+    };
+
+    input.click();
+}
+
+function generatePuzzle() {
+    const formData = new FormData();
+    const fileInput = document.getElementById('imageInput');
+    formData.append('image', fileInput.files[0]);
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('shareLink').value = data.link;
+    })
+    .catch(error => console.error('Error:', error));
+}
