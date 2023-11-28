4055b43525361ec76688d521fea0ac3d4248554b
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 12:25:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const Jimp = require('jimp');
 
 const app = express();
 const port = 3000;
@@ -14,13 +15,48 @@ app.use(express.static(path.join(__dirname, '../public')));
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), (req, res) => {
-    // Implement image processing logic and puzzle generation here
-    // For simplicity, let's just send back a sample link
-    const puzzleLink = 'https://example.com/puzzle?token=abc123';
-    res.json({ link: puzzleLink });
+app.post('/upload', upload.single('image'), async (req, res) => {
+    try {
+        const imageBuffer = req.file.buffer;
+
+        // Generate puzzle and get puzzle link
+        const puzzleLink = await generatePuzzle(imageBuffer);
+
+        res.json({ link: puzzleLink });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+async function generatePuzzle(imageBuffer) {
+    // Load the image using Jimp
+    const originalImage = await Jimp.read(imageBuffer);
+
+    // Resize the image to a smaller size (adjust as needed)
+    const resizedImage = originalImage.clone().resize(300, 200);
+
+    // Create a blank image for the puzzle
+    const puzzleImage = new Jimp(300, 200, 0xFFFFFFFF);
+
+    // Divide the image into a grid and shuffle the pieces
+    const gridSize = 3;
+    const pieceWidth = resizedImage.getWidth() / gridSize;
+    const pieceHeight = resizedImage.getHeight() / gridSize;
+
+    const pieces = [];
+    for (let y = 0; y < gridSize; y++) {
+        for (let x = 0; x < gridSize; x++) {
+            const piece = resizedImage.clone().crop(x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight);
+            pieces.push(piece);
+        }
+    }
+
+    // Shuffle the pieces
+    pieces.sort(() => Math.random() - 0.5);
+
+    // Paste the shuffled pieces onto the puzzle image
+    let currentX = 0;
+    let currentY = 0;
+
+    for (const piece of pieces) {
